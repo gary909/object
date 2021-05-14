da414f1817ccb6c917b77ea7619e0d04ed63a712
gary <gary.white@lodgit.com>
2021-05-14 17:11:25 +0200

    code updates

--- car.js
+++ car.js
@@ -1,4 +1,4 @@
-function CarConstructor (make, model, year, color, passengers, convertable, mileage) {
+function Car(make, model, year, color, passengers, convertable, mileage) {
     this.make = make;
     this.model = model;
     this.year = year;
@@ -22,3 +22,20 @@ function CarConstructor (make, model, year, color, passengers, convertable, mile
         }
     }
 };
+
+var chevy = new Car("Chevy", "Bel Air", 1957, "red", 2, false, 1021);
+var cadi = new Car("GM", "Cadillac", 1955, "tan", 5, false, 12892);
+var taxi = new Car("Webville Motors", "Taxi", 1955, "yellow", 4, false, 281241);
+var fiat = new Car("Fiat", "500", 1957, "Medium Blue", 2, false, 88000);
+
+var testCar = new Car("Webville Motors", "Test Car", 2014, "marine", 2, true, 21);
+
+var cars = [chevy, cadi, taxi, fiat, testCar];
+
+for(var i = 0; i < cars.length; i++) {
+    cars[i].start();
+    cars[i].drive();
+    cars[i].drive();
+    cars[i].stop();
+}
+
