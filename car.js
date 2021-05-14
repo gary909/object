2dd3e246e9d7990bcd45e98ae5e73146eb968522
gary <gary.white@lodgit.com>
2021-05-14 17:00:03 +0200

    code

--- car.js
+++ car.js
@@ -1,3 +1,24 @@
-function CarConstructor () {
+function CarConstructor (make, model, year, color, passengers, convertable, mileage) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.color = color;
+    this.passengers = passengers;
+    this.convertable = convertable;
+    this.mileage = mileage;
+    this.started = false;
 
+    this.start = function() {
+        this.started = true;
+    }
+    this.stop = function() {
+        this.started = false
+    }
+    this.drive = function() {
+        if (this.started) {
+            console.log(this.make + " " + this.model + " goes zoom zoom!");
+        } else {
+            console.log("Start the engine first.")
+        }
+    }
 };
